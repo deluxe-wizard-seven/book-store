ca57a0bf7a20276725b8c71502fd2ec4a4b3e9fd
deluxe-wizard-seven <diptobhattacharjee6@gmail.com>
2023-09-01 18:46:47 +0530

    Added the basic database connectivity to the book store app.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.listen(process.env.PORT, () => {
     console.log(`Book Store App is listening to port : ${process.env.PORT}`);
 });
+
+mongoose.connect(process.env.MONGO_DB_URI)
+        .then(() => {
+            console.log("Book Store App connected to the database.");
+        })
+        .catch((err) => {
+            console.error(err);
+        });
