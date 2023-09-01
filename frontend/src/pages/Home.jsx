9b8c5e277ba94f1992194e68a14a7d8e759d567c
deluxe-wizard-seven <diptobhattacharjee6@gmail.com>
2023-09-02 03:26:24 +0530

    Fixed the incorrect url endpoint issue.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -44,7 +44,7 @@ function Home() {
       </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
-        <Link to="/books/create">
+        <Link to="/books/add">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
